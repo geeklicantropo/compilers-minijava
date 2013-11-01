06aafaf7af14a2241f71739157089a50da7a059e
yurakuratov <yurakuratov@da9c940d-9ade-ba43-882b-6c619216b142>
2013-11-01 22:13:21 +0000

    убрал компаратор, все выводит

--- trunk/MiniJava/Symbols.cpp
+++ trunk/MiniJava/Symbols.cpp
@@ -1,6 +1,7 @@
 #include "Symbols.h"
 
-map<string, CSymbol*, CSymbol::comparer> CSymbol::storage;
+
+map<string, CSymbol*> CSymbol::storage;
 
 CSymbol::CSymbol( string _s ) :
 	s( _s ) {}
@@ -10,16 +11,8 @@ string CSymbol::getString() const
 	return s;
 }
 
-
-bool CSymbol::comparer::operator() (const string& first, const string& second)
-{
-	bool t = (first.compare( second ) == 0);
-	return first.compare( second ) == 0;
-}
-
 const CSymbol* CSymbol::CSymbolGet( string s )
 {
-	map<string, CSymbol*, CSymbol::comparer>::iterator it = storage.find( s );
 	if( storage.find( s ) != storage.end() )
 		return storage.find( s )->second;
 	CSymbol* tmp = new CSymbol( s );
