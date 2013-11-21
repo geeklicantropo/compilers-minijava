b32cc497f33fe0a59bce62dfa8e5d47844215208
alexandr.krasnyh <alexandr.krasnyh@da9c940d-9ade-ba43-882b-6c619216b142>
2013-11-21 00:13:04 +0000

    слегка начал проверку типов

--- MiniJava/TypeChecker.cpp
+++ MiniJava/TypeChecker.cpp
@@ -1,35 +1,109 @@
 #include "TypeChecker.h"
+#include "miniJava.h"
+#include "iostream"
 
+using std::cout;
 
-int Visit( const CProgram* n ) {return 0;}
-int Visit( const CMainClass* n ) {return 0;}
-int Visit( const CClassDeclareStar* n ) {return 0;}
-int Visit( const CClassDeclare* n ) {return 0;}
-int Visit( const CClassDeclareExtends* n ) {return 0;}
-int Visit( const CVarDeclareStar* n ) {return 0;}
-int Visit( const CVarDeclare* n ) {return 0;}
-int Visit( const CMethodDeclare* n ) {return 0;}
-int Visit( const CMethodDeclareStar* n ) {return 0;}
-int Visit( const CFormalList* n ) {return 0;}
-int Visit( const CFormalRestStar* n ) {return 0;}
-int Visit( const CStatement* n ) {return 0;}
-int Visit( const CStatementStar* n ) {return 0;}
-int Visit( const CStatementIf* n ) {return 0;}
-int Visit( const CStatementWhile* n ) {return 0;}
-int Visit( const CStatementSysOut* n ) {return 0;}
-int Visit( const CStatementAssignment* n ) {return 0;}
-int Visit( const CStatementArrayAssignment* n ) {return 0;}
-int Visit( const CExpressionBinOp* n ) {return 0;}
-int Visit( const CExpressionArray* n ) {return 0;}
-int Visit( const CExpressionLength* n ) {return 0;}
-int Visit( const CExpressionCallMethod* n ) {return 0;}
-int Visit( const CExpressionNumber* n ) {return 0;}
-int Visit( const CExpressionBool* n ) {return 0;}
-int Visit( const CExpressionVar* n ) {return 0;}
-int Visit( const CExpressionThis* n ) {return 0;}
-int Visit( const CExpressionNewInt* n ) {return 0;}
-int Visit( const CExpressionNewId* n ) {return 0;}
-int Visit( const CExpressionNegation* n ) {return 0;}
-int Visit( const CExpression* n ) {return 0;}
-int Visit( const CExpList* n ) {return 0;}
-int Visit( const CExpListNext* n ) {return 0;}
+CTypeChecker::CTypeChecker( CSymbolTable* st ): 
+	symbolTable( st ) {}
+
+int CTypeChecker::Visit( const CProgram* n ) 
+{
+	n->GetMainClass()->Accept( this );
+	if ( n->GetClassDeclareStar() != 0 ) n->GetClassDeclareStar()->Accept( this );
+	return 0;
+}
+
+int CTypeChecker::Visit( const CMainClass* n ) 
+{
+	n->GetStatement()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CClassDeclareStar* n ) 
+{
+	if ( n->GetClassDeclareStar() != 0 ) n->GetClassDeclareStar()->Accept( this );
+	n->GetClassDeclare()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CClassDeclare* n )
+{
+	if ( n->GetVarDeclareStar() != 0 ) n->GetVarDeclareStar()->Accept( this );
+	if ( n->GetMethodDeclareStar() != 0 ) n->GetMethodDeclareStar()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CClassDeclareExtends* n ) 
+{
+	if( n->GetVarDeclareStar() != 0) n->GetVarDeclareStar()->Accept( this );
+	if( n->GetMethodDeclareStar() != 0)  n->GetMethodDeclareStar()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CVarDeclareStar* n ) 
+{
+	if( n->GetVarDeclareStar() != 0 ) n->GetVarDeclareStar()->Accept( this );
+	n->GetVarDeclare()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CVarDeclare* n ) 
+{
+	return 0;
+}
+int CTypeChecker::Visit( const CMethodDeclare* n ) 
+{
+	if (n->GetFormalList() != 0) n->GetFormalList()->Accept( this );
+	if (n->GetVarDeclareStar() != 0) n->GetVarDeclareStar()->Accept( this );
+	if (n->GetStatementStar() != 0) n->GetStatementStar()->Accept( this );
+	n->GetExpression()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CMethodDeclareStar* n ) 
+{
+	if (n->GetMethodDeclareStar() != 0) n->GetMethodDeclareStar()->Accept( this );
+	n->GetMethodDeclare()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CFormalList* n ) 
+{
+	if (n->GetFormalRestStar() != 0) n->GetFormalRestStar()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CFormalRestStar* n ) 
+{
+	if (n->GetFormalRestStar() != 0) n->GetFormalRestStar()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CStatement* n ) 
+{
+	if (n->GetStatementStar() != 0) n->GetStatementStar()->Accept( this );
+	return 0;
+}
+int CTypeChecker::Visit( const CStatementStar* n ) {return 0;}
+int CTypeChecker::Visit( const CStatementIf* n ) {return 0;}
+int CTypeChecker::Visit( const CStatementWhile* n ) {return 0;}
+int CTypeChecker::Visit( const CStatementSysOut* n ) {return 0;}
+int CTypeChecker::Visit( const CStatementAssignment* n ) {return 0;}
+int CTypeChecker::Visit( const CStatementArrayAssignment* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionBinOp* n ) 
+{
+	if( !n->GetExpressionFirst()->Accept( this ) ) 
+		ErrorMessage(cout, "Left side must be of type integer", n->GetLocation() );
+	if( !n->GetExpressionSecond()->Accept( this ) ) 
+		ErrorMessage(cout, "Right side must be of type integer", n->GetLocation() );
+	return 0;
+}
+int CTypeChecker::Visit( const CExpressionArray* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionLength* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionCallMethod* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionNumber* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionBool* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionVar* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionThis* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionNewInt* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionNewId* n ) {return 0;}
+int CTypeChecker::Visit( const CExpressionNegation* n ) {return 0;}
+int CTypeChecker::Visit( const CExpression* n )
+{
+	//здесь то и нужно поюзать CSymbolTable
+	return 0;
+}
+int CTypeChecker::Visit( const CExpList* n ) {return 0;}
+int CTypeChecker::Visit( const CExpListNext* n ) {return 0;}
