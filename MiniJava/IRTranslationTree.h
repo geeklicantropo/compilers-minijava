0cf0aef0a857945e2f4d9a5754b3606864981952
yurakuratov <yurakuratov@da9c940d-9ade-ba43-882b-6c619216b142>
2013-12-05 11:27:50 +0000

    добавлены некоторые заголовки в IRTranslationTree.h

--- MiniJava/IRTranslationTree.h
+++ MiniJava/IRTranslationTree.h
@@ -1,10 +1,34 @@
 namespace IRTree {
 	class IExpression {
-		virtual void Visit();
+		virtual ~IExpression() {}
+		virtual void Accept( const IRTreeVisitor* v ) const;
+		virtual const CExpList* GetChild() const {}
 	};
 
 	class IStatement{
+		virtual ~IStatement() {}
+		virtual void Accept( const IRTreeVisitor* v ) const;
+		virtual const CStmList* GetChild() const {}
 	};
+};
+
+class IRTreeVisitor {
+	virtual ~IRTreeVisitor() {}
+	virtual void Visit( const CConst& p ) = 0;
+	virtual void Visit( const CName& p ) = 0;
+	virtual void Visit( const CTemp& p ) = 0;
+	virtual void Visit( const CBinop& p ) = 0;
+	virtual void Visit( const CMem& p ) = 0;
+	virtual void Visit( const CCall& p ) = 0;
+	virtual void Visit( const CEseq& p ) = 0;
+	virtual void Visit( const CMove& p ) = 0;
+	virtual void Visit( const CExp& p ) = 0;
+	virtual void Visit( const CJump& p ) = 0;
+	virtual void Visit( const CCJump& p ) = 0;
+	virtual void Visit( const CSeq& p ) = 0;
+	virtual void Visit( const CLabel& p ) = 0;
+	virtual void Visit( const CExpList& p ) = 0;
+	virtual void Visit( const CStmList& p ) = 0;
 };
 
 enum TBinop {
@@ -16,9 +40,15 @@ enum TCJump {
 };
 
 class CConst : IRTree::IExpression {
+public:
+	CConst( int v );
+	int GetValue() const;
+private:
+	int val;
 };
 
 class CName : IRTree::IExpression {
+	
 };
 
 class CTemp : IRTree::IExpression {
@@ -55,7 +85,25 @@ class CLabel : IRTree::IStatement {
 };
 
 class CExpList {
+public:
+	CExpList( const IRTree::IExpression* e, const CExpList* n );
+	const IRTree::IExpression* GetExp() const;
+	const CExpList* GetNext() const;
+	void SetExp( const IRTree::IExpression* e );
+	void SetNext( const CExpList* n );
+private:
+	const IRTree::IExpression* exp;
+	const CExpList* next;
 };
 
 class CStmList {
+public:
+	CStmList( const IRTree::IStatement* e, const CStmList* n );
+	const IRTree::IStatement* GetStm() const;
+	const CStmList* GetNext() const;
+	void SetStm( const IRTree::IStatement* e );
+	void SetNext( const CStmList* n );
+private:
+	const IRTree::IStatement* stm;
+	const CStmList* next;
 };
