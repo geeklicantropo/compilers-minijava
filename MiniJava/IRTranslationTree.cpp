a1df390209363e4beaaaa1c2868baf82bec84125
alexandr.krasnyh <alexandr.krasnyh@da9c940d-9ade-ba43-882b-6c619216b142>
2013-12-11 22:20:57 +0000

    part of IRTranslationTree.cpp

--- MiniJava/IRTranslationTree.cpp
+++ MiniJava/IRTranslationTree.cpp
@@ -62,3 +62,145 @@ const CExpList* CConst::GetChild() const
 {
 	return 0;
 }
+
+CName::CName(const CLabel* _label)
+{
+	label = _label;
+}
+
+const CLabel* CName::GetLabel() const 
+{
+	return label;
+}
+
+void CName::Accept( IRTreeVisitor* v ) const 
+{
+	v->Visit( *this );
+}
+
+const CExpList* CName::GetChild() const 
+{
+	return 0;
+}
+
+CTemp::CTemp( const Temp::CTemp* t ) {
+	temp = t;
+}
+
+const Temp::CTemp* CTemp::GetTemp() const
+{
+	return temp;
+}
+
+void CTemp::Accept( IRTreeVisitor* v ) const
+{
+	v->Visit( *this );
+}
+
+const CExpList* CTemp::GetChild() const
+{
+	return 0;
+}
+
+CBinOp::CBinOp( const IRTree::IExpression* _left, const IRTree::IExpression* _right, TBinOp _binop )
+{
+	left = _left;
+	right = _right;
+	binop = _binop;
+}
+
+TBinOp CBinOp::GetBinOp( ) const
+{
+	return binop;
+}
+
+const IRTree::IExpression* CBinOp::GetLeft() const
+{
+	return left;
+}
+
+const IRTree::IExpression* CBinOp::GetRight() const
+{
+	return right;
+}
+
+void CBinOp::Accept( IRTreeVisitor* v ) const
+{
+	return v->Visit( *this );
+}
+
+const CExpList* CBinOp::GetChild() const
+{
+	return new CExpList( left, new CExpList( right, 0 ) );
+}
+
+CMem::CMem( const IRTree::IExpression* e ) 
+{
+	exp = e;
+}
+
+const IRTree::IExpression* CMem::GetExp() const
+{
+	return exp;
+}
+
+void CMem::Accept( IRTreeVisitor* v ) const
+{
+	return v->Visit( *this );
+}
+
+const CExpList* CMem::GetChild() const
+{
+	return new CExpList( exp, 0 );
+}
+
+CCall::CCall( const IRTree::IExpression* f, const CExpList* a )
+{
+	func = f;
+	args = a;
+}
+
+const IRTree::IExpression* CCall::GetExp() const
+{
+	return func;
+}
+
+const CExpList* CCall::GetArgs() const
+{
+	return args;
+}
+
+void CCall::Accept( IRTreeVisitor* v ) const
+{
+	return v->Visit( *this );
+}
+
+const CExpList* CCall::GetChild() const
+{
+	return new CExpList( func, args );
+}
+
+CEseq::CEseq( const IRTree::IStatement* s, const IRTree::IExpression* e )
+{
+	stm = s;
+	exp = e;
+}
+const IRTree::IStatement* CEseq::GetStm() const
+{
+	return stm;
+}
+
+const IRTree::IExpression* CEseq::GetExp() const
+{
+	return exp;
+}
+
+void CEseq::Accept( IRTreeVisitor* v ) const
+{
+	return v->Visit( *this );
+}
+
+const CExpList* CEseq::GetChild() const
+{
+	return 0;
+}
