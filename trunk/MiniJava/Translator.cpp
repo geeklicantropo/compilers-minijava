1ff3cf6797ec6966890c75dcb9de5681cc8c6350
kostyan437@gmail.com <kostyan437@gmail.com@da9c940d-9ade-ba43-882b-6c619216b142>
2013-12-19 10:38:28 +0000

    some cosmetic

--- trunk/MiniJava/Translator.cpp
+++ trunk/MiniJava/Translator.cpp
@@ -120,35 +120,162 @@ const IRTree::IStatement* CRelativeCmpConverter::ToConditional( const Temp::CLab
     return 0;
 }
 
-int CTranslator::Visit( const CProgram* n ) {return 0;}
-int CTranslator::Visit( const CMainClass* n ){return 0;}
-int CTranslator::Visit( const CClassDeclareStar* n ){return 0;}
-int CTranslator::Visit( const CClassDeclare* n ){return 0;}
-int CTranslator::Visit( const CClassDeclareExtends* n ){return 0;}
-int CTranslator::Visit( const CVarDeclareStar* n ){return 0;}
-int CTranslator::Visit( const CVarDeclare* n ){return 0;}
-int CTranslator::Visit( const CMethodDeclare* n ){return 0;}
-int CTranslator::Visit( const CMethodDeclareStar* n ){return 0;}
-int CTranslator::Visit( const CFormalList* n ){return 0;}
-int CTranslator::Visit( const CFormalRestStar* n ){return 0;}
-int CTranslator::Visit( const CStatement* n ){return 0;}
-int CTranslator::Visit( const CStatementStar* n ){return 0;}
-int CTranslator::Visit( const CStatementIf* n ){return 0;}
-int CTranslator::Visit( const CStatementWhile* n ){return 0;}
-int CTranslator::Visit( const CStatementSysOut* n ){return 0;}
-int CTranslator::Visit( const CStatementAssignment* n ){return 0;}
-int CTranslator::Visit( const CStatementArrayAssignment* n ){return 0;}
-int CTranslator::Visit( const CExpressionBinOp* n ){return 0;}
-int CTranslator::Visit( const CExpressionArray* n ){return 0;}
-int CTranslator::Visit( const CExpressionLength* n ){return 0;}
-int CTranslator::Visit( const CExpressionCallMethod* n ){return 0;}
-int CTranslator::Visit( const CExpressionNumber* n ){return 0;}
-int CTranslator::Visit( const CExpressionBool* n ){return 0;}
-int CTranslator::Visit( const CExpressionVar* n ){return 0;}
-int CTranslator::Visit( const CExpressionThis* n ){return 0;}
-int CTranslator::Visit( const CExpressionNewInt* n ){return 0;}
-int CTranslator::Visit( const CExpressionNewId* n ){return 0;}
-int CTranslator::Visit( const CExpressionNegation* n ){return 0;}
-int CTranslator::Visit( const CExpression* n ){return 0;}
-int CTranslator::Visit( const CExpList* n ){return 0;}
-int CTranslator::Visit( const CExpListNext* n ){return 0;}
+int CTranslator::Visit( const CProgram* n ) 
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CMainClass* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CClassDeclareStar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CClassDeclare* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CClassDeclareExtends* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CVarDeclareStar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CVarDeclare* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CMethodDeclare* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CMethodDeclareStar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CFormalList* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CFormalRestStar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatement* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementStar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementIf* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementWhile* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementSysOut* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementAssignment* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CStatementArrayAssignment* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionBinOp* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionArray* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionLength* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionCallMethod* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionNumber* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionBool* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionVar* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionThis* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionNewInt* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionNewId* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpressionNegation* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpression* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpList* n )
+{
+	return 0;
+}
+
+int CTranslator::Visit( const CExpListNext* n )
+{
+	return 0;
+}
